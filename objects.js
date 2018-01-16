e0dcd85a00f01a6617984c7b08b227b0f9fdd7bc
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 01:27:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,3 +11,12 @@ function removeFromPlaylist (playlist, artistName) {
   return playlist
 }
   
+function updatePlaylist(playlist, artist, song) {
+  // Remember, because one of this function's arguments
+  // is `playlist`, the `playlist` _inside_ the function
+  // is not the same as the top-level `playlist` outside
+  // the function
+  playlist[artist] = song
+
+  return playlist
+}
