e1a0f8c40eb5aeddcd170f17bcdf477d9c979b56
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 01:29:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artist] = song
   return playlist
 }
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist(playlist, artist) {
   delete playlist[artist]
   
   return playlist
